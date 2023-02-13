fb2411583059072d8a2224ddfb039e08786cdf73
Ner Gaon, Roi <roing65@gmail.com>
2023-02-13 17:08:43 +0200

    Add client and sever for app game

--- client.py
+++ client.py
@@ -9,13 +9,12 @@ while True:
     guess = int(input("Enter your guess: "))
 
     # Send the guess to the server
-    response = requests.post(url, form={"guess": guess})
+    response = requests.post(url, data={"guess": guess})
 
     # Check if the user has won the game
     if "Well done" in response.text:
         print(response.text)
         break
-
 
     # Print the result from the server
     print(response.text)
