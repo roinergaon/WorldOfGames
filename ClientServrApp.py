d9767b11e4f86a39c6ac75fe396d6128799bd2d9
Ner Gaon, Roi <roing65@gmail.com>
2023-02-13 18:08:53 +0200

    Add client and sever for app game

--- ClientServrApp.py
+++ ClientServrApp.py
@@ -1,23 +1,33 @@
-import sys
+from flask import Flask, request
 import random
-from flask import Flask, render_template, request
+import sys
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    message = "Welcome to the number guessing game!"
-    secret = random.randint(0, 100) if len(sys.argv) < 2 else random.randint(0, int(sys.argv[1]))
-    if request.method == 'POST':
-        guess = int(request.form['guess'])
-        if guess == secret:
-            message = "Well done! The secret number was {}".format(secret)
-        elif guess > secret:
-            message = "Too big! Try again."
-        else:
-            message = "Too small! Try again."
-    return render_template('index.html', message=message)
+# Welcome message for the user
+@app.route('/')
+def welcome():
+    return "Welcome to the number guessing game!\nGuess a number between 0 and 100."
 
+# Generate a random number in the range [0-100]
+secret = random.randint(0, 100)
+
+# Get the user's guess and compare it to the secret number
+@app.route('/guess', methods=['POST'])
+def guess():
+    guess = int(request.form['guess'])
+    if guess > secret:
+        return "Too big"
+    elif guess < secret:
+        return "Too small"
+    else:
+        return "Well done"
+
+# Check if the number of arguments passed through sys.args is correct
+if len(sys.argv) != 2:
+    print("Usage: python number_guesser.py PORT")
+    sys.exit()
+
+# Start the Flask server
 if __name__ == '__main__':
-    app.run()
-
+    app.run(port=int(sys.argv[1]), debug=True)
