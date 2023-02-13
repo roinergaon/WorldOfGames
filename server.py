d4b3751e945391c6d15d318f2983a3669ce2538f
Ner Gaon, Roi <roing65@gmail.com>
2023-02-13 17:11:44 +0200

    Add client and sever for app game

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Generate a random number in range [0, 100]
 secret = random.randint(0, 100)
 
-@app.route("/guess", methods=["POST"])
+@app.route("/guess", methods=["GET"])
 def guess():
     # Get the guess from the client
     guess = int(request.form["guess"])
